f9fb4edd87670677409fe2700964cc13e67e4a55
Kwon O Tae <62225124+oteosuk@users.noreply.github.com>
2023-11-21 16:23:47 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A * B;
+
+
+
+    return 0;
+}
