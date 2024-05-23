09b14b8177471addd4c85f5be2d2373f6915250a
Kwon O Tae <62225124+oteosuk@users.noreply.github.com>
2024-05-23 22:07:18 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n)
+{
+    int count = 0;
+    
+    while (n > 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n -= 1;
+            count++;
+        }
+    }
+    
+    return count;
+}
