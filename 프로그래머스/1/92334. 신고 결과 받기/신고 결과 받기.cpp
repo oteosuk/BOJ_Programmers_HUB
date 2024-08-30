a5609d98d77edd60b5b89351229a7b3fb5c03cee
Kwon O Tae <62225124+oteosuk@users.noreply.github.com>
2024-08-30 12:27:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 312.98 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -3,22 +3,21 @@
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-    // 1.
+
     const int n = id_list.size();
-    map<string, int> Conv;
+    map<string, int> Conv; // 사람, 인덱스
     for (int i = 0; i < n; i++) Conv[id_list[i]] = i;
 
-    // 2.
     vector<pair<int, int>> v;
     sort(report.begin(), report.end());
     report.erase(unique(report.begin(), report.end()), report.end());
     for (const auto& s : report) {
         stringstream in(s);
         string a, b; in >> a >> b;
+        //신고한사람인덱스 신고받은사람인덱스
         v.push_back({ Conv[a], Conv[b] });
     }
 
-    // 3.
     vector<int> cnt(n), ret(n);
     for (const auto& [a, b] : v) cnt[b]++;
     for (const auto& [a, b] : v) if (cnt[b] >= k) ret[a]++;
